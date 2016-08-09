3ef3bc76362d647d0fcb97794952d2a0e70cd39b
vit <vit@canaima-popular.(none)>
2016-08-09 16:01:01 -0430

    vue vacie

---
+++ src/js/app.js
@@ -0,0 +1,32 @@
+var App = Vue.extend({});
+
+var postList = Vue.extend({
+    template:'#post-list-template',
+    data: function(){
+        return {
+            posts: ''
+        }
+    },
+
+    ready: function(){
+        posts = this.$http.get('http://localhost/voilarts/wp-json/wp/v2/posts');
+
+        posts.then(function(posts){
+            this.$set('posts', posts);
+            console.log(posts);
+        })
+    }
+})
+
+
+
+var router = new VueRouter();
+
+router.map({
+    '/':{
+        component: postList
+    }
+});
+
+router.start(App, '#app');
+
